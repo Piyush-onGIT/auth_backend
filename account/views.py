1be7355ca399ffd3d2d097e764d078b488075004
Aniket Kumar <kraniket123654@gmail.com>
2022-12-26 19:41:02 +0530

    token generation done

--- account/views.py
+++ account/views.py
@@ -4,6 +4,16 @@ from rest_framework.views import APIView
 from account.serializers import UserRegistrationSerializer, UserLoginSerializer
 from django.contrib.auth import authenticate
 from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# Generate Token Manually
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 class UserRegistrationViews(APIView):
     renderer_classes = [UserRenderer]
@@ -11,7 +21,8 @@ class UserRegistrationViews(APIView):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-            return Response({'msg':'Registration Successful'},
+            token = get_tokens_for_user(user)
+            return Response({'token':token, 'msg':'Registration Successful'},
             status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -24,6 +35,7 @@ class UserLoginView(APIView):
             password = serializer.data.get('password')
             user = authenticate(email=email, password=password)
             if user is not None:
-                return Response({'msg':'Login Success'}, status=status.HTTP_200_OK)
+                token = get_tokens_for_user(user)
+                return Response({'token':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
             else:
                 return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
