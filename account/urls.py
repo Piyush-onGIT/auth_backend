89e7e36977e98655397ae84a0587998bb0e96cfa
Aniket Kumar <kraniket123654@gmail.com>
2022-12-26 20:15:24 +0530

    user profile api added

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from account.views import UserRegistrationViews, UserLoginView
+from account.views import UserRegistrationViews, UserLoginView, UserProfileView
 
 urlpatterns = [
     path('register/', UserRegistrationViews.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
